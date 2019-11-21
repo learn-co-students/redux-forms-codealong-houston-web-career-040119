8b5f74e0f1abdaa65591f9b7ee146ba576437ecf
github username <github email address>
2019-11-20 23:49:12 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import CreateTodo from './components/todos/CreateTodo'
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <CreateTodo />
+        <CreateTodo store={this.props.store} />
       </div>
     );
   }
 }
-
 export default App;
